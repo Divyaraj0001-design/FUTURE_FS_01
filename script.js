0b48e41850f720b90b53f17b6648773f0f356d10
Divya Raj <rajdivya6533@gmail.com>
2026-02-01 00:42:53 +0530

    Add files via upload
    Initial portfolio upload

---
+++ script.js
@@ -0,0 +1,27 @@
+document.getElementById("btn").onclick = function () {
+  document.getElementById("projects").scrollIntoView({
+    behavior: "smooth"
+  });
+};
+// Active navbar link on scroll
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 150;
+    if (scrollY >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === `#${current}`) {
+      link.classList.add("active");
+    }
+  });
+});
+
